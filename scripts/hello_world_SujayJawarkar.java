84ad354ad78230631659ce12e62db030f7a69266
Sujay Jawarkar <sujayp175@gmail.com>
2024-10-01 19:11:03 +0530

    Create hello_world_SujayJawarkar.java

---
+++ scripts/hello_world_SujayJawarkar.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Sujay Jawarkar
+// GITHUB: https://github.com/SujayJawarkar
+
+public class myFirstPull{
+  public static void main(Strings[] args){
+  System.out.println("Hello World");
+}
+}
